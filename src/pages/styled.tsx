b68a32e98af033b7abbfbd52d99b3a71729a8139
ianlapham <ianlapham@gmail.com>
2021-07-12 18:05:29 -0400

    update with latest

---
+++ src/pages/styled.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components/macro'
+
+export const StandardPageWrapper = styled.div`
+  padding-top: 160px;
+  width: 100%;
+`
+
+export const IframeBodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 1rem;
+  align-items: center;
+  flex: 1;
+  z-index: 1;
+`
